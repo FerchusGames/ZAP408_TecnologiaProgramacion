acf64ce4cbb7acb2675987f856355dbab1655536
Ferchus <ferchusgames@protonmail.com>
2022-02-23 07:59:51 -0600

    Añadir solución "ZAP408_Encapsulamiento_y_Herencia"

---
+++ ZAP408_Encapsulamiento_y_Herencia/ZAP408_Encapsulamiento_y_Herencia.cpp
@@ -0,0 +1,19 @@
+// ZAP408_Encapsulamiento_y_Herencia.cpp : Esta vez, haremos clases con herencia para entender estos dos pilares de la POO.
+
+#include <string>
+#include <iostream>
+
+int main()
+{
+
+}
+
+class Ropa
+{
+public:
+	float precio;
+	std::string materiales;
+
+private:
+
+};
