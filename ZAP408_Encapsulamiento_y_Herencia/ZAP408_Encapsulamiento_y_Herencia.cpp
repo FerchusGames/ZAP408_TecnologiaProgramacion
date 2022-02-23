444719fbd910426db9a4f6298b0c1142e7fad851
Ferchus <ferchusgames@protonmail.com>
2022-02-23 08:50:49 -0600

    Versión 1 del código

--- ZAP408_Encapsulamiento_y_Herencia/ZAP408_Encapsulamiento_y_Herencia.cpp
+++ ZAP408_Encapsulamiento_y_Herencia/ZAP408_Encapsulamiento_y_Herencia.cpp
@@ -3,17 +3,56 @@
 #include <string>
 #include <iostream>
 
-int main()
-{
-
-}
-
 class Ropa
 {
 public:
 	float precio;
-	std::string materiales;
+	int nivelSwag;
+	std::string materiales[5];
+	std::string parteDelCuerpo;
+	std::string nombre;
+	std::string talla;
+	std::string color;
+	bool antiBalas;
+	bool impermeable;
+
+	void Presumir(std::string a) //Para presumirle a los demás de tu facha
+	{
+		std::cout << "*Le presume " << a << " *" << std::endl;
+	}
 
 private:
 
 };
+
+class Playera:public Ropa //Es una clase derivada de Ropa
+{
+public:
+	bool mangas;
+	bool estampado;
+	bool requiereBaterias;
+};
+
+int main()
+{
+	Playera fiestasDeOctubre;
+	fiestasDeOctubre.requiereBaterias = true;
+	fiestasDeOctubre.estampado = true;
+	fiestasDeOctubre.mangas = true;
+	fiestasDeOctubre.precio = 50.00f;
+	fiestasDeOctubre.nivelSwag = 11;
+	fiestasDeOctubre.materiales[0] = "Algodón";
+	fiestasDeOctubre.materiales[1] = "Litio";
+	fiestasDeOctubre.materiales[2] = "Mucho amor";
+	fiestasDeOctubre.parteDelCuerpo = "Torso";
+	fiestasDeOctubre.nombre = "Playera con lucecitas watchinango";
+	fiestasDeOctubre.talla = "XXL";
+	fiestasDeOctubre.color = "Cafe";
+	fiestasDeOctubre.antiBalas = false;
+	fiestasDeOctubre.impermeable = false;
+
+	fiestasDeOctubre.Presumir(fiestasDeOctubre.nombre);
+
+	return 0;
+}
+
