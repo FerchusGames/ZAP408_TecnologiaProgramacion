e905219dce62951af5071f6cefcc26ec273d7354
Ferchus <ferchusgames@protonmail.com>
2022-02-23 07:56:47 -0600

    Antes de eliminar .cpp de herencia

---
+++ ZAP408_TecnologiaProgramacion/ZAP408_Encapsulamiento_y_Herencia.cpp
@@ -0,0 +1,13 @@
+// ZAP408_Encapsulamiento_y_Herencia.cpp : Esta vez, haremos clases con herencia para entender estos dos pilares de la POO.
+
+#include <string>
+
+class Ropa
+{
+public:
+		float precio;
+		std::string materiales;
+
+private: 
+
+};
