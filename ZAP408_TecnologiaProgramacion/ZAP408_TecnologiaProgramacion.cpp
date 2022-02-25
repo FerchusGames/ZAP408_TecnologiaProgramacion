7b7b7a9fff81b203c0ee946033617aca64c0f295
Ferchus <ferchusgames@protonmail.com>
2022-02-25 10:27:35 -0600

    Before the purge

--- ZAP408_TecnologiaProgramacion/ZAP408_TecnologiaProgramacion.cpp
+++ ZAP408_TecnologiaProgramacion/ZAP408_TecnologiaProgramacion.cpp
@@ -1,8 +1,29 @@
 // ZAP408_TecnologíaProgramación.cpp : Este es el proyecto que estaremos usando durante toda el semestre.
 
+// Este programa cuenta el número de vocales que tiene con punteros.
+
 #include <iostream>
+#include <string>
 
 int main()
 {
+	
+	std::string palabra;
+	char vocales[5] = { 'a', 'e', 'i', 'o', 'u' };
+	std::string* palabraPtr = &palabra;
+	
 
+	std::cin >> palabra;
+	std::cout << "Memory Adress:" << palabraPtr << std::endl;
+	std::cout << "Data: " << *palabraPtr << std::endl;
+	std::cout << "Memory Adress:" << vocalesPtr << std::endl;
+
+	std::cout << *vocalesPtr[4] << std::endl;
+
+
+	for (int i = 0; i < 5; i++) 
+	{
+		std::cout << *vocalesPtr[i] << std::endl;
+	}
+	
 }
