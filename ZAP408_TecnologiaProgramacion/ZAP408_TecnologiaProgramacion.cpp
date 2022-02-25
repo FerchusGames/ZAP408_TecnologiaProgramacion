df7d187bd3b685fab828c663267a0146a5b38e7f
Ferchus <ferchusgames@protonmail.com>
2022-02-25 10:41:50 -0600

    Punteros V1

--- ZAP408_TecnologiaProgramacion/ZAP408_TecnologiaProgramacion.cpp
+++ ZAP408_TecnologiaProgramacion/ZAP408_TecnologiaProgramacion.cpp
@@ -7,23 +7,23 @@
 
 int main()
 {
-	
 	std::string palabra;
-	char vocales[5] = { 'a', 'e', 'i', 'o', 'u' };
 	std::string* palabraPtr = &palabra;
-	
+	char vocales[10] = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+	int numVocales = 0;
+	int* numVocalesPtr = &numVocales;
 
-	std::cin >> palabra;
-	std::cout << "Memory Adress:" << palabraPtr << std::endl;
-	std::cout << "Data: " << *palabraPtr << std::endl;
-	std::cout << "Memory Adress:" << vocalesPtr << std::endl;
-
-	std::cout << *vocalesPtr[4] << std::endl;
-
-
-	for (int i = 0; i < 5; i++) 
+	std::cin >> *palabraPtr;
+	for (int i = 0; i < palabra.length(); i++)
 	{
-		std::cout << *vocalesPtr[i] << std::endl;
+		for (int j = 0; j < 10; j++)
+		{
+			if (palabra[i] == vocales[j])
+			{
+				*numVocalesPtr = *numVocalesPtr + 1;
+			}
+		}
 	}
-	
+
+	std::cout << "The number of vowels is: " << numVocales;
 }
