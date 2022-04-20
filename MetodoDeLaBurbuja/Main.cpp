158d7c2a61cd0834895959017e8ad84b03083f80
Ferchus <ferchusgames@protonmail.com>
2022-04-20 09:39:21 -0500

    Progreso Clase

---
+++ MetodoDeLaBurbuja/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <locale>
+#define MAX 
+
+int main()
+{
+	int arraySize
+}
+
+void llenarArreglo(int arraySize, int arreglo[])
+{
+	for (int i = 0; i < arraySize; i++)
+	{
+		std::cout << "Introduce el elemento " << i << ": ";
+		std::cin >> arreglo[i];
+	}
+}
